a64fb3ceb6459d5c094e1180a843851b52f0eda2
Alex Olson <olsonalexw@gmail.com>
2026-06-01 08:44:26 -0400

    bootstrap state bucket

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "x0lie-analytics-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
